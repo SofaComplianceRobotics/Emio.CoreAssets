cf42e6be0669dcc7b44eca7ce963fcd5ee9e17b1
EulalieCoevoet <eulalie.coevoet@gmail.com>
2025-09-12 17:04:11 +0200

    Update test_imguibindings.py

--- tests/test_imguibindings.py
+++ tests/test_imguibindings.py
@@ -4,18 +4,18 @@ import subprocess
 
 
 def test_imguibindings():
-    nbIterations = 10
     filename = os.path.dirname(os.path.abspath(__file__)) + "/../labs/introduction/introduction.py"
     argv = "--no-connection"
 
     if os.path.isfile(filename):
         stderr = ""
-        args = ["runSofa", filename, "-l", "SofaPython3,SofaImGui", "-g", "imgui", "-n", str(nbIterations)]
+        args = ["runSofa", filename, "-l", "SofaPython3,SofaImGui", "-g", "imgui"]
         if argv is not None:
             args += ["--argv", argv]
         try:
-            subprocess.run(args, capture_output=True, timeout=1, text=True, encoding="utf-8")
+            subprocess.run(args, capture_output=True, timeout=10, text=True, encoding="utf-8")
         except subprocess.TimeoutExpired as e:
+            assert "Finished validating" in str(e.stdout)
             for output in [e.stdout, e.stderr]:
                 assert "[ERROR]" not in str(output)
                 assert "segfault" not in str(output)
