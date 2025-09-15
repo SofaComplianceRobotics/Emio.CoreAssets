fedf748af75320509b7d38ba12f90b7963fc18bb
EulalieCoevoet <eulalie.coevoet@gmail.com>
2025-09-15 15:36:43 +0200

    Update test_imguibindings.py

--- tests/test_imguibindings.py
+++ tests/test_imguibindings.py
@@ -7,19 +7,18 @@ def test_imguibindings():
     if platform != "win32":
         # This test is not supported on the CI on Windows (the driver does not support OpenGL)
         filename = os.path.dirname(os.path.abspath(__file__)) + "/../labs/introduction/introduction.py"
-        argv = "--no-connection"
-    
         if os.path.isfile(filename):
-            stderr = ""
-            args = ["runSofa", filename, "-l", "SofaPython3,SofaImGui", "-g", "imgui"]
-            if argv is not None:
-                args += ["--argv", argv]
+            timeoutRaised = False
+            args = ["runSofa", filename, "-l", "SofaPython3,SofaImGui", "-g", "imgui", "--argv", "--no-connection"]
             try:
                 subprocess.run(args, capture_output=True, timeout=10, text=True, encoding="utf-8")
             except subprocess.TimeoutExpired as e:
+                timeoutRaised = True
                 assert "Finished validating" in str(e.stdout)
                 for output in [e.stdout, e.stderr]:
                     assert "[ERROR]" not in str(output)
                     assert "segfault" not in str(output)
+            if not timeoutRaised:
+                raise RuntimeError("The process runSofa did not run as expected.")
         else:
             raise FileNotFoundError(filename)
